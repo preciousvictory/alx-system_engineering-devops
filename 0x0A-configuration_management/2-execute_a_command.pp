63d404061cad6a8d30fc2cd158c92c0232f9e588
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-24 14:53:12 -0700

    configuration management

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Using Puppet, create a manifest that kills a process named killmenow.
-exec {'pkill':
-  command  => 'pkill',
+exec { 'pkill':
+  command  => 'pkill killmenow',
   provider => 'shell',
 }
