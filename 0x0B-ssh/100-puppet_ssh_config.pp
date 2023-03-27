091a39cb1ae058fe5f6cbd1095f2dc02bda34065
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-27 06:54:31 -0700

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,4 @@
-#using Puppet to make changes to our configuration file
-file {'~/.ssh/config':
-	ensure => present,
-	path   => '/etc/ssh/ssh_config'
-}
-
-exec { 'change ~/.ssh/config':
-	command  => 'eciho "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
-	provider => 'shell',
-	path     => '/bin/'
+# setup client SSH configuration
+exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
+        path    => '/bin/'
 }
