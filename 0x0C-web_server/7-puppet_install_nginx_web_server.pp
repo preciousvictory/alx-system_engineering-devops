1ad5ce4cae5aad53e621cd345b0243e8d4e3d1c3
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-28 10:48:43 -0700

    web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 # install and configure an Nginx server using Puppet instead of Bash.
 exec { 'server configuration':
-	command => 'apt-get -y update; apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.nginx-debian.html; sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default; service nginx start'
+	command  => 'apt-get -y update; apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.nginx-debian.html; sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default; service nginx start',
 	provider => shell,
 }
