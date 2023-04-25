a4d56dfa4b5af4c88d3edd9b15e42d96874fd56a
preciousvictory <victoryexcel2018@gmail.com>
2023-04-24 19:26:24 -0700

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
-""" a Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
+"""
+a Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
 import json
 import urllib.request
 import sys
 
 
-id = sys.argv[1]
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        id_ = sys.argv[1]
+        url = 'https://jsonplaceholder.typicode.com/'
+        with urllib.request.urlopen('{}users?id={}'.format(url, id_)) as f:
+            user = json.loads(f.read().decode())
+            user_name = user[0].get("name")
 
-with urllib.request.urlopen('https://jsonplaceholder.typicode.com/users?id={}'.format(id)) as f:
-    user = json.loads(f.read().decode())
-    user_name = user[0].get("name")
+        if user_name is not None:
+            with urllib.request.urlopen('{}todos?userId={}'
+                                        .format(url, id_)) as f:
+                response = json.loads(f.read().decode())
+                c = 0
+                count_T = 0
+                titles = []
+                for i in response:
+                    c += 1
 
-with urllib.request.urlopen('https://jsonplaceholder.typicode.com/todos?userId={}'.format(id)) as f:
-    response = json.loads(f.read().decode())
-    c = 0
-    count_T = 0
-    titles = []
-    for i in response:
-        c += 1
-        if i.get("completed") == True:
-            count_T += 1
-            titles.append(i.get("title"))
+                    if i.get("completed") is True:
+                        count_T += 1
+                        titles.append(i.get("title"))
 
-print(f'Employee {user_name} is done with tasks({count_T}/{c}):')
-for i in titles:
-    print(f'     {i}')
+            print(f'Employee {user_name} is done with tasks({count_T}/{c}):')
+            for i in titles:
+                print(f'     {i}')
