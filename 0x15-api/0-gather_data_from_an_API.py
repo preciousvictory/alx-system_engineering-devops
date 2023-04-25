b573eaf83b9e6d8cb44c88265eb19b5ec3a7daf5
preciousvictory <victoryexcel2018@gmail.com>
2023-04-24 19:34:17 -0700

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,8 +3,8 @@
 a Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
-import urllib.request
 import sys
+import urllib.request
 
 
 if __name__ == "__main__":
