2f0ad4fe918078c42ac891368cafad475e8111a6
preciousvictory <victoryexcel2018@gmail.com>
2023-04-24 19:58:55 -0700

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,32 +4,21 @@ a Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
 
-import json
+import requests
 import sys
-import urllib.request
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        id_ = sys.argv[1]
-        url = 'https://jsonplaceholder.typicode.com/'
-        with urllib.request.urlopen('{}users?id={}'.format(url, id_)) as f:
-            user = json.loads(f.read().decode())
-            user_name = user[0].get("name")
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get("{}users?id={}".format(url, sys.argv[1])).json()
+    todos = requests.get('{}todos?userId={}'.format(url, sys.argv[1])).json()
 
-        if user_name is not None:
-            with urllib.request.urlopen('{}todos?userId={}'
-                                        .format(url, id_)) as f:
-                response = json.loads(f.read().decode())
-                c = 0
-                count_T = 0
-                titles = []
-                for i in response:
-                    c += 1
+    completed = []
+    for t in todos:
+        if t.get("completed") is True:
+            completed.append(t.get("title"))
+    name = user[0].get("name")
 
-                    if i.get("completed") is True:
-                        count_T += 1
-                        titles.append(i.get("title"))
-
-            print(f'Employee {user_name} is done with tasks({count_T}/{c}):')
-            for i in titles:
-                print(f'\t {i}')
+    print("Employee {} is done with tasks({}/{}):".format(name,
+        len(completed), len(todos)))
+    for c in completed:
+        print("\t {}".format(c))
---
+++ 0x15-api/0-gather_data_from_an_API_.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+a Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+
+import json
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        id_ = sys.argv[1]
+        url = 'https://jsonplaceholder.typicode.com/'
+        with urllib.request.urlopen('{}users?id={}'.format(url, id_)) as f:
+            user = json.loads(f.read().decode())
+            user_name = user[0].get("name")
+
+        if user_name is not None:
+            with urllib.request.urlopen('{}todos?userId={}'
+                                        .format(url, id_)) as f:
+                response = json.loads(f.read().decode())
+                c = 0
+                count_T = 0
+                titles = []
+                for i in response:
+                    c += 1
+
+                    if i.get("completed") is True:
+                        count_T += 1
+                        titles.append(i.get("title"))
+
+            print(f'Employee {user_name} is done with tasks({count_T}/{c}):')
+            for i in titles:
+                print(f'\t {i}')
