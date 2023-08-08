cd91a8ec0a557ceabe88c3db6f52ae1f4d0f5c15
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-08-08 05:37:26 -0700

    APIs

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,7 +4,8 @@ import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed for a given subreddit."""
+    """prints the titles of the first 10 hot posts listed for a
+    given subreddit"""
     headers = {'User-Agent': 'preciousvicky_'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
