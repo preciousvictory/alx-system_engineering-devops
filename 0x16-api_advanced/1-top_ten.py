cef4fad8be69b782c4e78f6b2b021cf3957b3d1a
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-08-08 05:34:55 -0700

    APIs

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Function that queries the Reddit API """
+import requests
+
+
+def top_ten(subreddit):
+    """ prints the titles of the first 10 hot posts listed for a given subreddit."""
+    headers = {'User-Agent': 'preciousvicky_'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    res = response.json()
+    if response.status_code == 200:
+        for hot in res.get('data').get('children'):
+            print(hot.get('data').get('title'))
+        return
+    print('None')
