50b6b7d6bb71edd99dc267c9d1173655d869bee1
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-08-17 08:20:51 -0700

    web stack debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # fix our stack so that we get to 0 errors
 exec { 'file limit':
-	command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
-	provider => shell,
+  command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
+  provider => shell,
 }
