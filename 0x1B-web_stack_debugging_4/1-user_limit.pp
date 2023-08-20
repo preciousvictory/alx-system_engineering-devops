3dff31b13a56dfb920f77dec1a58ca4c1c637c26
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-08-20 04:47:48 -0700

    web stack debugging

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,12 @@
 # login with the holberton user and open a file without any error message
 exec { 'Change soft limit':
-  command => 'sudo sed -i "/holberton soft/s/4/10000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
+  command  => 'sudo sed -i "/holberton soft/s/4/10000/" /etc/security/limits.conf',
+  path     => '/usr/local/bin/:/bin/',
   provider => shell,
 }
 
 exec { 'Change hard limit':
-  command => 'sudo sed -i "/holberton hard/s/5/100000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
+  command  => 'sudo sed -i "/holberton hard/s/5/100000/" /etc/security/limits.conf',
+  path     => '/usr/local/bin/:/bin/',
   provider => shell,
 }
